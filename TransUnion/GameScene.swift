95f5c6ababf926b45012648f467e64113fee493f
mwong56 <michael.wong110@gmail.com>
2016-05-06 22:21:55 -0700

    Add Title screen and background moving

--- TransUnion/GameScene.swift
+++ TransUnion/GameScene.swift
@@ -8,7 +8,13 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var moving:SKNode!
+    var score = NSInteger()
+    var started = false
+    var myLabel:SKLabelNode!
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
@@ -17,25 +23,70 @@ class GameScene: SKScene {
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
         self.addChild(myLabel)
+        
+
+
+        
+        
+        
+        
     }
+    
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
-            let location = touch.locationInNode(self)
+//            let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//
+//            sprite.xScale = 0.5
+//            sprite.yScale = 0.5
+//            sprite.position = location
+//            
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
+//            
+//            self.addChild(sprite)
+        }
+        
+        if (!started) {
+            self.removeAllChildren()
+            setupGame();
+            started = true;
+        }
+        
+    }
+    
+    func setupGame() {
+        // setup physics
+        self.physicsWorld.gravity = CGVector( dx: 0.0, dy: -5.0 )
+        self.physicsWorld.contactDelegate = self
+        
+        // set up moving node
+        moving = SKNode()
+        self.addChild(moving)
+        
+        // background image
+        let skyTexture = SKTexture(imageNamed: "Spaceship")
+        skyTexture.filteringMode = .Nearest
+        
+        let moveSkySprite = SKAction.moveByX(-skyTexture.size().width * 2.0, y: 0, duration: NSTimeInterval(0.1 * skyTexture.size().width * 2.0))
+        let resetSkySprite = SKAction.moveByX(skyTexture.size().width * 2.0, y: 0, duration: 0.0)
+        let moveSkySpritesForever = SKAction.repeatActionForever(SKAction.sequence([moveSkySprite,resetSkySprite]))
+        
+        var i:CGFloat = 0
+        
+        while (i < CGFloat(2.0 + self.frame.size.width / ( skyTexture.size().width * 2.0 ))) {
+            let sprite = SKSpriteNode(texture: skyTexture)
+            sprite.setScale(2.0)
+            sprite.zPosition = 0;
+            sprite.position = CGPoint(x: i * sprite.size.width, y: sprite.size.height / 2)
+            sprite.runAction(moveSkySpritesForever)
+            moving.addChild(sprite)
+            i++
         }
     }
    
