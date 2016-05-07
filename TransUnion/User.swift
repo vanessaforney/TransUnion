aec1e8720c3f579b752ef3721ce52b03de7728b3
Vanessa Forney <vanessaforney@gmail.com>
2016-05-06 19:12:16 -0700

    Adds fonts

---
+++ TransUnion/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  TransUnion
+//
+//  Created by Vanessa Forney on 5/6/16.
+//  Copyright © 2016 Vanessa Forney. All rights reserved.
+//
+
+import Foundation
+
+public class User {
+
+  var date: String = ""
+  var name: String = ""
+  var state: String = ""
+  var score: Int = 0
+  var total: Float = 0
+
+}
