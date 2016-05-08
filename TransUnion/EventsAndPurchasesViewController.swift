e1538974a6e114d498cedfab9f78b1b76785fe2a
RussFenenga <russfenenga@gmail.com>
2016-05-07 18:06:43 -0700

    More work on screens.

---
+++ TransUnion/EventsAndPurchasesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EventsAndPurchasesViewController.swift
+//  TransUnion
+//
+//  Created by Russ Fenenga on 5/7/16.
+//  Copyright © 2016 Vanessa Forney. All rights reserved.
+//
+
+import UIKit
+
+class EventsAndPurchasesViewController: UIViewController {
+    
+    @IBOutlet weak var actualLoanBalance: UILabel!
+    @IBOutlet weak var numberOfEvents: UILabel!
+    @IBOutlet weak var numberOfPurchases: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
